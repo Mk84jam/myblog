98f8b0967faa542fc4bf45e83cd108ed02bc4841
Mk84jam <55362038+Mk84jam@users.noreply.github.com>
2021-05-22 18:04:37 +0200

    update 1.0

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Blog from './components/Blog.js';
 
 function App() {
   return (
     <div className="App">
+      <div className="App-main-box">
+        <Blog />
+      </div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>My First React Deployment</h1>
+        
       </header>
     </div>
   );
---
+++ src/components/Blog.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Blog.css';
+
+function Blog () {
+
+return(
+<div>
+
+<b><u>This is a Blog</u></b>
+<p>Testing 123</p>
+
+
+</div>
+
+);
+
+}
+
+
+export default Blog;
